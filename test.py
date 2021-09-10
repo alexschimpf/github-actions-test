18fa070076c407d5abeee2c30e161c25b80c3400
Alex Schimpf <alex.schimpf@xad.com>
2021-09-10 14:08:21 -0700

    fix

--- test.py
+++ test.py
@@ -7,10 +7,15 @@ MAX_TIMEOUT = 60
 start_time = time.time()
 while time.time() - start_time < MAX_TIMEOUT:
     print('Attempting request...')
-    response = requests.get('http://localhost:1080/mockserver/dashboard')
-    if response.status_code == 200:
-        print('Success!')
-        sys.exit()
+
+    try:
+        response = requests.get('http://localhost:1080/mockserver/dashboard')
+    except Exception:
+        pass
+    else:
+        if response.status_code == 200:
+            print('Success!')
+            sys.exit()
 
     print('Sleeping...')
     time.sleep(5)
